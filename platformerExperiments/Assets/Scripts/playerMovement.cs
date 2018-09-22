e97a63d6f5d3df539c11c3eaa7e77bdb9dc58e41
Dolphinos <33300007+Dolphinos7@users.noreply.github.com>
2018-09-22 16:42:50 -0400

    Got movment, jumping, and limited max speed workig

--- platformerExperiments/Assets/Scripts/playerMovement.cs
+++ platformerExperiments/Assets/Scripts/playerMovement.cs
@@ -3,7 +3,8 @@ public class playerMovement : MonoBehaviour {
     // Use this for initialization
     bool keyHeld = false;
     bool onGround = false;
-        void OnCollisionEnter2D(UnityEngine.Collision2D collisionInfo)
+    float maxSpeed = 6f;
+    void OnCollisionEnter2D(UnityEngine.Collision2D collisionInfo)
     {
         if (collisionInfo.gameObject.tag == "Ground")
         {
@@ -16,12 +17,19 @@ public class playerMovement : MonoBehaviour {
 
     void Start ()
     {
-        
+
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        if (gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > maxSpeed)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity.normalized * maxSpeed;
+        }
+        Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+        Debug.Log(currentVelocity);
         keyHeld = false;
+        gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * -500 * Time.deltaTime);
         if (Input.GetKey("space")&&onGround==true)
         {
             onGround = false;
@@ -31,12 +39,12 @@ public class playerMovement : MonoBehaviour {
         if (Input.GetKey("d"))
         {
             keyHeld = true;
-            FindObjectOfType<Rigidbody2D>().velocity = transform.right * 120 * Time.deltaTime;
+            FindObjectOfType<Rigidbody2D>().AddForce(transform.right * 1000 * Time.deltaTime);
         }
         if (Input.GetKey("a"))
         {
             keyHeld = true;
-            FindObjectOfType<Rigidbody2D>().velocity = transform.right * -120 * Time.deltaTime;
+            FindObjectOfType<Rigidbody2D>().AddForce(transform.right * -1000 * Time.deltaTime);
         }
         if(keyHeld==false&&onGround==true)
             FindObjectOfType<Rigidbody2D>().velocity = transform.up * 0 * Time.deltaTime;

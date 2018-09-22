de65d156fad3ceba21a734774dfbf919564fb052
Dolphinos <33300007+Dolphinos7@users.noreply.github.com>
2018-09-21 23:32:11 -0400

    added movement by setting velocity

---
+++ platformerExperiments/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class playerMovement : MonoBehaviour {
+    // Use this for initialization
+    bool keyHeld = false;
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKey("d"))
+        {
+            FindObjectOfType<Rigidbody2D>().velocity = transform.right * 120 * Time.deltaTime;
+        }
+        else if (Input.GetKey("a"))
+        {
+            FindObjectOfType<Rigidbody2D>().velocity = transform.right * -120 * Time.deltaTime;
+        }
+        else
+            FindObjectOfType<Rigidbody2D>().velocity = transform.up * 0;
+    }
+}

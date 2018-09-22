8156aa5e6060e63873924ef44e216ff7bcd1c6d7
Dolphinos <33300007+Dolphinos7@users.noreply.github.com>
2018-09-22 15:54:52 -0400

    Moving and jumping working
    Works properly, however while in air if you move you stop falling.

--- platformerExperiments/Assets/Scripts/playerMovement.cs
+++ platformerExperiments/Assets/Scripts/playerMovement.cs
@@ -2,22 +2,43 @@
 public class playerMovement : MonoBehaviour {
     // Use this for initialization
     bool keyHeld = false;
-	void Start () {
+    bool onGround = false;
+        void OnCollisionEnter2D(UnityEngine.Collision2D collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+
+
+
+
+    void Start ()
+    {
         
-	}
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
+        keyHeld = false;
+        if (Input.GetKey("space")&&onGround==true)
+        {
+            onGround = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 10000 * Time.deltaTime);
+            
+        }
         if (Input.GetKey("d"))
         {
+            keyHeld = true;
             FindObjectOfType<Rigidbody2D>().velocity = transform.right * 120 * Time.deltaTime;
         }
-        else if (Input.GetKey("a"))
+        if (Input.GetKey("a"))
         {
+            keyHeld = true;
             FindObjectOfType<Rigidbody2D>().velocity = transform.right * -120 * Time.deltaTime;
         }
-        else
-            FindObjectOfType<Rigidbody2D>().velocity = transform.up * 0;
+        if(keyHeld==false&&onGround==true)
+            FindObjectOfType<Rigidbody2D>().velocity = transform.up * 0 * Time.deltaTime;
     }
 }

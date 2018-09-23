3f5c6993fdc4f87c5d16a2f4862c7da986097a71
pdflynn <pdflynn@users.noreply.github.com>
2018-09-22 23:24:20 -0400

    idek at this point

---
+++ platformerExperiments/Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (getPlayerPositionY() < -10)
+        {
+            gameOver();
+        }
+        Debug.Log(getPlayerPositionY());
+	}
+    /**
+     * Returns the current value of the player-character's Y position
+     */
+    public float getPlayerPositionY()
+    {
+        return GameObject.Find("testSprite").transform.position.y;
+    }
+    /**
+     * Procedure for what happens when the game is over
+     */
+     public void gameOver()
+    {
+        Debug.Log("Game Over!");
+        SceneManager.LoadScene("levelTesting");
+    }
+}
+
+    
---
+++ platformerExperiments/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private float speed;
+	// Use this for initialization
+	void Start () {
+        speed = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get user input
+        float axisX = Input.GetAxis("Horizontal");
+        float axisY = Input.GetAxis("Vertical");
+
+        //move the player left/right - vertical is not used
+        transform.Translate(new Vector3(axisX, 0) * Time.deltaTime * speed);
+
+        
+	}   
+}
